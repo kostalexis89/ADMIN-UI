2d5a415382efac02c35a79a9987a5f95fb06230a
kostalexis89 <kostalexis.hosting@gmail.com>
2022-01-20 16:37:20 +0100

    rendered tags to the table for every app

--- src/pages/AppEntries.js
+++ src/pages/AppEntries.js
@@ -12,12 +12,21 @@ export default function AppEntries() {
     setAppList(appData.data);
     setTagList(tagsData.data);
   }, []);
+
   const result = appList.map((app, index) => {
+    for (let i = 0; i < app.tagsID.length; i++) {
+      console.log(app.tagsID[i]);
+      for(let j=0; j< tagList.length;j++){
+        if(tagList[j].id===app.tagsID[i]){
+            app.tagsID[i] = tagList[j].name + "   "
+        }
+      }
+    }
     return (
       <>
         {/* <p key={app.id}>{app.name}</p> */}
         <tr key={app.id}>
-          <td>{index+1}</td>
+          <td>{index + 1}</td>
           <td>{app.name}</td>
           <td>{app.tagsID}</td>
           <td>Delete Edit</td>
@@ -36,9 +45,7 @@ export default function AppEntries() {
           <th>Actions</th>
         </tr>
       </thead>
-      <tbody>
-        {result}
-      </tbody>
+      <tbody>{result}</tbody>
     </Table>
   );
 }
